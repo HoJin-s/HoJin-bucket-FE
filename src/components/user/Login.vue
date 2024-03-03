55235a8d68c96c2c325f0443056e6de10bab8438
hjzheld <dusen7774@naver.com>
2024-03-03 22:26:06 +0900

    Add: css 수정 및 회원가입 페이지로 이동 추가

--- src/components/user/Login.vue
+++ src/components/user/Login.vue
@@ -1,21 +1,23 @@
 <template>
-    <div class="login-wrap">
-        <button class="close-button">X</button>
-        <div class="login">
-            <div class="login-info">
-                <h1>로그인</h1>
-            </div>
-            <div class="login-user">
-                <p>닉네임</p>
-                <input />
-                <p>비밀번호</p>
-                <input />
-            </div>
-            <form>
-                <button class="login-button">로그인</button>
-            </form>
-            <div>
-                <button class="go-signup">회원가입 페이지로 →</button>
+    <div class="back-color">
+        <div class="login-wrap" v-if="user_open == 1">
+            <button @click="$emit('closeLogin')" class="close-button">X</button>
+            <div class="login">
+                <div class="login-info">
+                    <h1>로그인</h1>
+                </div>
+                <div class="login-user">
+                    <p>닉네임</p>
+                    <input />
+                    <p>비밀번호</p>
+                    <input />
+                </div>
+                <form>
+                    <button class="login-button">로그인</button>
+                </form>
+                <div>
+                    <button @click="$emit('goSignup')" class="go-signup">회원가입 페이지로 →</button>
+                </div>
             </div>
         </div>
     </div>
@@ -27,5 +29,8 @@ import "@/assets/css/user/login.css"
 
 export default {
     name: 'LoginPage',
+    props : {
+        user_open: Number,
+    },
 }
 </script>
